fcfd9e3b1874a14ce869699be22c4df3da04db45
Bryan Muñoz Gutierrez <bryanpotoso99@gmail.com>
2023-10-24 13:05:15 -0600

    i

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const ul = document.querySelector(".nav__ul");
 const ulNesting1 = document.querySelector(".nav__nesting1 ");
 const ulNesting2 = document.querySelector(".nav__nesting2 ");
-const menu = document.querySelector(".icono__main");
+const ulNesting = document.querySelector(".nav__nesting");
+// const menu = document.querySelector(".icono__main");
+
+document.querySelector(".icono__main").addEventListener("click", () => {
+  ul.classList.toggle("activo");
+});
 
 document.querySelector(".link1").addEventListener("click", function () {
   ulNesting1.classList.toggle("activo");
@@ -10,6 +15,11 @@ document.querySelector(".link2").addEventListener("click", function () {
   ulNesting2.classList.toggle("activo");
 });
 
-document.querySelector(".icono__main").addEventListener("click", () => {
-  ul.classList.toggle("activo");
-});
+/* function abrirMenu(e) {
+  if (e.target.contains(".nav__links")) {
+    ulNesting.classList.toggle("activo");
+  }
+  console.log(`Has dado click en ${e.target}`);
+}
+ul.addEventListener("click", abrirMenu);
+*/
