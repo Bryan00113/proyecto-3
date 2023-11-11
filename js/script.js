afc36120cc43527a7456b7d251f389df5a5fce71
Bryan Muñoz Gutierrez <bryanpotoso99@gmail.com>
2023-11-11 08:56:08 -0600

    Este es el primer commit de este archivo

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ const ul = document.querySelector(".nav__ul");
 const ulNesting1 = document.querySelector(".nav__nesting1 ");
 const ulNesting2 = document.querySelector(".nav__nesting2 ");
 const ulNesting = document.querySelector(".nav__nesting");
+const buycart = document.querySelector(".buy__cart");
 // const menu = document.querySelector(".icono__main");
 
-document.querySelector(".icono__main").addEventListener("click", () => {
+document.querySelector(".icon__main").addEventListener("click", () => {
   ul.classList.toggle("activo");
+});
+document.querySelector(".icon__close").addEventListener("click", () => {
+  ul.classList.toggle("activo");
+});
+document.querySelector(".icon__cart").addEventListener("click", () => {
+  buycart.classList.toggle("activo");
 });
 
 document.querySelector(".link1").addEventListener("click", function () {
@@ -15,11 +22,10 @@ document.querySelector(".link2").addEventListener("click", function () {
   ulNesting2.classList.toggle("activo");
 });
 
-/* function abrirMenu(e) {
-  if (e.target.contains(".nav__links")) {
-    ulNesting.classList.toggle("activo");
-  }
-  console.log(`Has dado click en ${e.target}`);
-}
-ul.addEventListener("click", abrirMenu);
-*/
+// function abrirMenu(e) {
+//   if (e.target.contains(".nav__links")) {
+//     ulNesting.classList.toggle("activo");
+//   }
+//   console.log(`Has dado click en ${e.target}`);
+// }
+// ul.addEventListener("click", abrirMenu);
